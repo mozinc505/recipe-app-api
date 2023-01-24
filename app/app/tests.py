6b2e9dcab3f388b7cc929e9a100efd266dbfd9be
Matjaz <matjaz.mozina@minoa.si>
2023-01-24 10:04:52 +0100

    Adding support for Postgresql (v2)

--- app/app/tests.py
+++ app/app/tests.py
@@ -6,21 +6,18 @@ from django.test import SimpleTestCase
 
 from app import calc
 
-
-
 class CalcTests(SimpleTestCase):
     """Test the calc module."""
 
     """Test metoda se mora začeti z besedico test_"""
     def test_add_number(self):
         """Test adding numbers together."""
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
 
-        self.assertEqual(res,11)
-
+        self.assertEqual(res, 11)
         
     def test_subtract_numbers(self):
         """Test substracting numbers."""
-        res = calc.subtract(10,15)
+        res = calc.subtract(10, 15)
 
         self.assertEqual(res, 5)
